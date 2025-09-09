fa9b538599753cd5bcb69828f26154af28e1e53b
Valentinaruiz22 <202215.clv@gmail.com>
2025-09-08 21:48:24 -0500

    Primer commit del proyecto React

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,9 +3,9 @@ import type { Product } from '../types';
 export const PRODUCTS: Product[] = [
   {
     TITULO: 'Pañal Winny',
-    DESCRIPCION: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+    DESCRIPCION: 'Ideales para recién nacidos, estos pañales están diseñados para cuidar la piel delicada del bebé desde el primer día.',
     OPCIONES: 'Etapa 0',
-    PRECIO: 24100,
+    PRECIO: 23600,
     DETALLEPRECIO: 'paquete de 30',
     FOTO: 'https://megatiendas.vtexassets.com/arquivos/ids/166669-800-450?v=638386983779400000&width=800&height=450&aspect=true',
     CATEGORIA: 'Higiene',
@@ -13,9 +13,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Pañal Winny',
     DESCRIPCION:
-      'Pañales ultra suaves diseñados especialmente para niñas, con tecnología de absorción rápida y ajuste perfecto',
+      'Diseñados para bebés de más de 13 kg, ofrecen comodidad y protección durante el día y la noche.',
     OPCIONES: 'Etapa 6',
-    PRECIO: 30000,
+    PRECIO: 40000,
     DETALLEPRECIO: 'paquete de 50',
     FOTO: 'https://www.elmundodelaura.com/cdn/shop/files/WINNY_GOLD_ET_6_X_50.jpg?v=1737666259',
     CATEGORIA: 'Higiene',
@@ -23,9 +23,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Pañal Winny',
     DESCRIPCION:
-      'Pañales ultra suaves diseñados especialmente para niñas, con tecnología de absorción rápida y ajuste perfecto',
+      'Perfectos para bebés de más de 13 kg, estos pañales tipo pants ofrecen libertad de movimiento y protección duradera.',
     OPCIONES: 'Etapa 6',
-    PRECIO: 30000,
+    PRECIO: 60000,
     DETALLEPRECIO: 'paquete de 30',
     FOTO: 'https://www.elmundodelaura.com/cdn/shop/files/WINNY_GOLD_ET_6_X_30.jpg?v=1737665849',
     CATEGORIA: 'Higiene',
@@ -33,9 +33,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Pañal Winny',
     DESCRIPCION:
-      'Pañales ultra suaves diseñados especialmente para niñas, con tecnología de absorción rápida y ajuste perfecto',
+      'Indicados para bebés entre 5.5 y 7 kg, ofrecen protección, suavidad y comodidad durante sus primeros meses.',
     OPCIONES: 'Etapa 2',
-    PRECIO: 30000,
+    PRECIO: 99000,
     DETALLEPRECIO: 'paquete de 100',
     FOTO: 'https://www.elmundodelaura.com/cdn/shop/products/PA_ALES_WINNY_ULTRATRIM_2_X_100.jpg?v=1662045381',
     CATEGORIA: 'Higiene',
@@ -43,9 +43,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Pañal Winny',
     DESCRIPCION:
-      'Pañales ultra suaves diseñados especialmente para niñas, con tecnología de absorción rápida y ajuste perfecto',
+      'Indicados para bebés entre 7 y 10 kg, estos pañales ofrecen absorción eficiente y comodidad para el día y la noche.',
     OPCIONES: 'Etapa 3',
-    PRECIO: 30000,
+    PRECIO: 130000,
     DETALLEPRECIO: 'paquete de 100',
     FOTO: 'https://www.elmundodelaura.com/cdn/shop/files/WINNY_GOLD_ET_6_X_30.jpg?v=1737665849',
     CATEGORIA: 'Higiene',
@@ -53,28 +53,27 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Pañal Winny',
     DESCRIPCION:
-      'Pañales ultra suaves diseñados especialmente para niñas, con tecnología de absorción rápida y ajuste perfecto',
+      'Pensados para bebés entre 9 y 12 kg, ofrecen protección prolongada y comodidad para el día y la noche.',
     OPCIONES: 'Etapa 4',
-    PRECIO: 30000,
+    PRECIO: 150000,
     DETALLEPRECIO: 'paquete de 100',
     FOTO: 'https://www.elmundodelaura.com/cdn/shop/files/ULT-ETAPA-4-X-100.jpg?v=1697052854',
     CATEGORIA: 'Higiene',
   },
   {
     TITULO: 'Toallitas Húmedas Premium',
-    DESCRIPCION:
-      'Toallitas súper suaves con extractos naturales de manzanilla y aloe vera, libres de alcohol y parabenos',
-    OPCIONES: 'pack de 3 paquetes',
-    PRECIO: 18000,
-    DETALLEPRECIO: '3x80 unidades',
+    DESCRIPCION:'Ideales para limpieza suave y efectiva en manos, cara y cuerpo del bebé.',
+    OPCIONES: 'un paquete',
+    PRECIO: 12000,
+    DETALLEPRECIO: '80 unidades',
     FOTO: 'https://f.fcdn.app/imgs/6c8f81/www.sanroque.com.uy/sanruy/8717/original/catalogo/128189_128189_1/1920-1200/toallitas-humedas-premium-wetclean-x-80-unidades-toallitas-humedas-premium-wetclean-x-80-unidades.jpg',
     CATEGORIA: 'Higiene',
   },
   {
     TITULO: 'Toallitas Húmedas Winny',
     DESCRIPCION:
-      'Toallitas súper suaves con extractos naturales de manzanilla y aloe vera, libres de alcohol y parabenos',
-    OPCIONES: 'paquete 100 unidades',
+      'Ideales para la limpieza diaria del bebé, estas toallitas son suaves, hipoalergénicas y enriquecidas con ingredientes calmantes.',
+    OPCIONES: 'un paquete',
     PRECIO: 18000,
     DETALLEPRECIO: '100 unidades',
     FOTO: 'https://locatelcolombia.vtexassets.com/arquivos/ids/334832/7701021145949_2_Oferta-Winny-Toallitas-Humedas-Aloe-Y-Vitamina-E-X-100Und.jpg?v=638152079282500000',
@@ -83,9 +82,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Crema Antipañalitis',
     DESCRIPCION:
-      'Crema protectora con óxido de zinc que previene y trata la dermatitis del pañal de forma natural',
-    OPCIONES: 'tubo 100ml',
-    PRECIO: 25000,
+      'Crema antipañalitis formulada con óxido de zinc, ideal para prevenir y aliviar irritaciones en la piel del bebé.',
+    OPCIONES: '20g',
+    PRECIO: 10000,
     DETALLEPRECIO: 'unidad',
     FOTO: 'https://http2.mlstatic.com/D_NQ_NP_992830-MLA80530714946_112024-O.webp',
     CATEGORIA: 'Cuidado',
@@ -93,9 +92,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Shampoo Bebé Sin Lágrimas',
     DESCRIPCION:
-      'Fórmula ultra suave que no irrita los ojos del bebé, con ingredientes naturales para cabello delicado',
+      'Formulado especialmente para el cuero cabelludo sensible del bebé, limpia suavemente sin causar irritación ocular.',
     OPCIONES: 'frasco 250ml',
-    PRECIO: 22000,
+    PRECIO: 39000,
     DETALLEPRECIO: 'unidad',
     FOTO: 'https://hiperlibertad.vtexassets.com/arquivos/ids/230042/SHAMPOO-PARA-BEBE-JOHNSON-S-CABELLO-CLARO-x-400-ML-2-49962.jpg?v=638687614048730000',
     CATEGORIA: 'Cuidado',
@@ -103,9 +102,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Tetero Anticólicos',
     DESCRIPCION:
-      'Biberón con sistema anticólicos que reduce la ingesta de aire, fabricado en materiales libres de BPA',
+      'Diseñado para reducir cólicos, gases y reflujo, gracias a su válvula AirFree que evita la entrada de aire.',
     OPCIONES: '150ml - 250ml',
-    PRECIO: 35000,
+    PRECIO: 30000,
     DETALLEPRECIO: 'unidad',
     FOTO: 'https://tse3.mm.bing.net/th/id/OIP.Z3JUveSPWVI6U1wcFZOSswHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Alimentación',
@@ -113,9 +112,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Papilla Orgánica Frutas',
     DESCRIPCION:
-      'Papilla natural hecha con frutas orgánicas, sin conservantes ni azúcares añadidos, ideal para bebés desde 6 meses',
+      'Ideal para bebés a partir de los 6 meses, esta papilla combina frutas 100% orgánicas sin aditivos artificiales.',
     OPCIONES: 'pack de 6 frascos',
-    PRECIO: 42000,
+    PRECIO: 40000,
     DETALLEPRECIO: '6x113g',
     FOTO: 'https://tse1.mm.bing.net/th/id/OIP.rYFEimX0ICjpCFSzo5PRmAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Alimentación',
@@ -123,49 +122,48 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Baberos Impermeables',
     DESCRIPCION:
-      'Set de baberos con diseños divertidos, fáciles de limpiar y ajustables para mayor comodidad',
-    OPCIONES: 'set de 5 unidades',
-    PRECIO: 28000,
-    DETALLEPRECIO: '5 unidades',
+      'Diseñados para proteger la ropa del bebé durante las comidas, con materiales resistentes y fáciles de limpiar.',
+    OPCIONES: 'unidad',
+    PRECIO: 11000,
+    DETALLEPRECIO: 'unidad',
     FOTO: 'https://tse3.mm.bing.net/th/id/OIP.slM1Trnx99WsQywg6reNUwHaHQ?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Accesorios',
   },
   {
     TITULO: 'Baberos Tela',
     DESCRIPCION:
-      'Set de baberos con diseños divertidos, fáciles de limpiar y ajustables para mayor comodidad',
-    OPCIONES: '1 unidades',
-    PRECIO: 10000,
-    DETALLEPRECIO: '1 unidades',
+      'Babero suave y absorbente, ideal para uso diario en bebés durante la lactancia o alimentación.',
+    OPCIONES: 'por unidad',
+    PRECIO: 8000,
+    DETALLEPRECIO: '1 unidad',
     FOTO: 'https://http2.mlstatic.com/D_NQ_NP_748764-MCO44623275452_012021-O.webp',
     CATEGORIA: 'Accesorios',
   },
   {
     TITULO: 'Baberos Silicona',
     DESCRIPCION:
-      'Set de baberos con diseños divertidos, fáciles de limpiar y ajustables para mayor comodidad',
-    OPCIONES: '1 unidades',
-    PRECIO: 20000,
-    DETALLEPRECIO: '1 unidades',
+      'Diseñados para facilitar la hora de comer sin desastres, estos baberos son impermeables, flexibles y muy fáciles de limpiar.',
+    OPCIONES: 'por unidad',
+    PRECIO: 15000,
+    DETALLEPRECIO: '1 unidad',
     FOTO: 'https://exitocol.vtexassets.com/arquivos/ids/26414872/baberos-para-bebe-en-silicona-facil-lavado-color-salmon.jpg?v=638730744678100000',
     CATEGORIA: 'Accesorios',
   },
   {
     TITULO: 'Kid de aseo',
     DESCRIPCION:
-      'Set de baberos con diseños divertidos, fáciles de limpiar y ajustables para mayor comodidad',
+      'Kit de Aseo para Bebé de 8 piezas, ideal para cubrir las necesidades básicas de higiene desde el nacimiento',
     OPCIONES: '8 piezas',
-    PRECIO: 50000,
+    PRECIO: 40000,
     DETALLEPRECIO: '1 kid',
     FOTO: 'https://tiendamavinel.com/wp-content/uploads/2022/04/kid-aseo-azul.png',
     CATEGORIA: 'Cuidado',
   },
   {
     TITULO: 'Sonajero Educativo',
-    DESCRIPCION:
-      'Juguete sensorial que estimula el desarrollo motor y auditivo del bebé, fabricado con materiales seguros',
-    OPCIONES: 'varios colores',
-    PRECIO: 15000,
+    DESCRIPCION:'Juguete ideales para estimular el desarrollo sensorial y motriz de tu bebé.',
+    OPCIONES: 'varios modelos',
+    PRECIO: 25000,
     DETALLEPRECIO: 'unidad',
     FOTO: 'https://tse2.mm.bing.net/th/id/OIP.ndh0tp5ZYLc3CLC-6c_j_gHaF7?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Juguetes',
@@ -173,9 +171,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Manta Térmica Bebé',
     DESCRIPCION:
-      'Manta suave y cálida perfecta para mantener la temperatura ideal del bebé, hipoalergénica y lavable',
+      'Diseñada para brindar abrigo suave y seguro, ideal para cuna, coche o momentos de descanso.',
     OPCIONES: '80cm x 100cm',
-    PRECIO: 45000,
+    PRECIO: 50000,
     DETALLEPRECIO: 'unidad',
     FOTO: 'https://tse3.mm.bing.net/th/id/OIP._IvUtK1KUMJXTVdyCU67vwHaF6?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Textiles',
@@ -183,9 +181,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Body Manga Larga',
     DESCRIPCION:
-      'Bodies de algodón 100% orgánico, súper suaves y cómodos para el día a día del bebé',
-    OPCIONES: 'tallas 0-24 meses',
-    PRECIO: 32000,
+      'Diseñada para brindar abrigo suave y seguro, ideal para cuna, coche o momentos de descanso.',
+    OPCIONES: 'todas las tallas',
+    PRECIO: 60000,
     DETALLEPRECIO: 'pack de 3',
     FOTO: 'https://tse3.mm.bing.net/th/id/OIP.vYIQJNIBtpSHaJoTHGCHfwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Ropa',
@@ -193,9 +191,9 @@ export const PRODUCTS: Product[] = [
   {
     TITULO: 'Chupones Ortodónticos',
     DESCRIPCION:
-      'Chupetes diseñados para el desarrollo oral saludable, libres de BPA con diseños coloridos',
-    OPCIONES: '0-6 meses, 6-18 meses',
-    PRECIO: 20000,
+      'Diseñado para bebés desde el nacimiento hasta los 6 meses, este chupón ofrece comodidad, seguridad y ventilación óptima.',
+    OPCIONES: '0-6 meses',
+    PRECIO: 45000,
     DETALLEPRECIO: 'pack de 2',
     FOTO: 'https://tse1.mm.bing.net/th/id/OIP.lTJ47ymWbldTk5vt6qwB7wHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
     CATEGORIA: 'Accesorios',
